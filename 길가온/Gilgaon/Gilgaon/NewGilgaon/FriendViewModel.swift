aa8289b967a5d08c122f2d5adfb80a4db04dcd16
stealmh <kmh922@naver.com>
2023-03-02 16:42:30 +0900

    [Add] FriendViewModel 생성 #31

---
+++ 길가온/Gilgaon/Gilgaon/NewGilgaon/FriendViewModel.swift
@@ -0,0 +1,125 @@
+//
+//  FriendViewModel.swift
+//  Gilgaon
+//
+//  Created by 김민호 on 2023/03/02.
+//
+
+import Foundation
+import Firebase
+import FirebaseAuth
+
+class FriendViewModel: ObservableObject {
+    @Published var myInfo: FriendModel?
+    @Published var friendInfo: FriendModel?
+    @Published var friendRequestArray: [FriendModel] = []
+    @Published var friendRequestArrayUid: [String] = []
+    @Published var listener: ListenerRegistration?
+    let database = Firestore.firestore()
+    //상대방이 추가할 때 내 아이디에 친구의 uid를 담아준다.
+    
+    //나의 친구요청목록을 확인할 때
+    func fetchFriendRequest(){
+        print(#function)
+        guard let uid = Auth.auth().currentUser?.uid else { return }
+        let ref = database.collection("Waiting").document(uid)
+        listener = ref.addSnapshotListener { querySnapshot, error in
+            guard let document = querySnapshot  else { return }
+            guard let docData = document.data() else { return }
+            self.friendRequestArrayUid = docData["sendToFriend"] as? [String] ?? []
+        }
+    }
+    
+    
+    //[sendFriendRequest를 위한 함수] 친구에게 온 친구목록을 조회
+    private func fetchWaitingFriend(_ userId: String) async -> [String]? {
+        print(#function)
+        do{
+            let document = try await database.collection("Waiting").document(userId).getDocument()
+            guard let docData = document.data() else{ return [] }
+            let waitingArr = docData["sendToFriend"] as? [String] ?? []
+            return waitingArr
+        }catch{
+            print("Error!")
+            return nil
+        }
+    }
+    //친구요청을 보낼 때
+    func sendFriendRequest(friendUid: String) async {
+        print(#function)
+        guard let myUid = Auth.auth().currentUser?.uid else { return }
+        guard var friendsWaitingArray = await self.fetchWaitingFriend(friendUid) else {return}
+        friendsWaitingArray.append(myUid)
+        print("없니..?",friendsWaitingArray)
+        //친구의 uid에 내 uid를 추가함
+        do {
+            print("do문 입니다.")
+            try await database
+                .collection("Waiting")
+                .document(friendUid)
+                .setData(["sendToFriend": friendsWaitingArray])
+        } catch {
+            print("== sendFriendRequest error ==")
+        }
+    }
+    //친구를 추가할 때
+    func addFriendBoth() {
+        
+    }
+    
+    //친구추가
+    func addFriend() {
+        guard let myInfo, let friendInfo else {return}
+        database
+            .collection("User") //
+            .document(myInfo.id)
+            .collection("Friend")
+            .document(friendInfo.id)
+            .setData(["id": friendInfo.id,
+                      "nickName": friendInfo.nickName,
+                      "userPhoto": friendInfo.userPhoto,
+                      "userEmail": friendInfo.userEmail])
+        database
+            .collection("User") //
+            .document(friendInfo.id)
+            .collection("Friend")
+            .document(myInfo.id)
+            .setData(["id": myInfo.id,
+                      "nickName": myInfo.nickName,
+                      "userPhoto": myInfo.userPhoto,
+                      "userEmail": myInfo.userEmail])
+    }
+    
+    //사용자로부터 닉네임을 입력받아 일치하는 유저를 조회하는 함수
+    func findUserUId(userUid: String, myUid: String) async{
+        database
+            .collection("User")
+            .getDocuments { (snapshot, error) in
+//                self.userList.removeAll()
+                if let snapshot{
+                    for document in snapshot.documents{
+                        let id: String = document.documentID
+                        if id == userUid {
+                            let docData = document.data()
+                            let nickName: String = docData["nickName"] as? String ?? ""
+                            let userPhoto: String = docData["userPhoto"] as? String ?? ""
+                            let userEmail:String = docData["userEmail"] as? String ?? ""
+                            let friend = FriendModel(id: id, nickName: nickName, userPhoto: userPhoto, userEmail: userEmail)
+                            self.friendInfo = friend
+                            print("userUid일때:",friend.nickName)
+                        }
+                        else if id == myUid {
+                            let docData = document.data()
+                            let nickName: String = docData["nickName"] as? String ?? ""
+                            let userPhoto: String = docData["userPhoto"] as? String ?? ""
+                            let userEmail:String = docData["userEmail"] as? String ?? ""
+                            let friend = FriendModel(id: id, nickName: nickName, userPhoto: userPhoto, userEmail: userEmail)
+                            self.myInfo = friend
+                            print("나의Uid일때:",friend.nickName)
+                        }
+                        
+                    }
+                }
+            }
+    }
+}
