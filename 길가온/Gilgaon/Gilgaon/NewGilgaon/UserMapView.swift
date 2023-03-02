7788cd329bb8008ad1e8c62db4443b8beaeb8704
Sehoon Jeong <wtd120120@gmail.com>
2023-03-02 11:49:18 +0900

    [Add] UserMapView 파일 생성 #32

---
+++ 길가온/Gilgaon/Gilgaon/NewGilgaon/UserMapView.swift
@@ -0,0 +1,20 @@
+//
+//  UserMapView.swift
+//  Gilgaon
+//
+//  Created by sehooon on 2023/03/02.
+//
+
+import SwiftUI
+
+struct UserMapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct UserMapView_Previews: PreviewProvider {
+    static var previews: some View {
+        UserMapView()
+    }
+}
