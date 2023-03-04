d92fb136f5593dc6f678dd1dc005f563e786791a
JIN-JJS <joonsu2005@naver.com>
2023-03-05 04:44:34 +0900

    [Feat] 애플 로그인 연결 (애플 로그인 -> 회원가입 View로 이동하여 회원가입 완료)

--- 길가온/Gilgaon/Gilgaon/View/LoginView.swift
+++ 길가온/Gilgaon/Gilgaon/View/LoginView.swift
@@ -1,26 +1,6 @@
 import SwiftUI
 import AuthenticationServices
 
-struct AppleUser: Codable {
-    let userId: String
-    let firstName: String
-    let lastName: String
-    let email: String
-    
-    init?(credentials: ASAuthorizationAppleIDCredential) {
-        guard
-            let firstName = credentials.fullName?.givenName,
-            let lastName = credentials.fullName?.familyName,
-            let email = credentials.email
-        else { return nil }
-        
-        self.userId = credentials.user
-        self.firstName = firstName
-        self.lastName = lastName
-        self.email = email
-    }
-}
-
 struct LoginView: View {
     @EnvironmentObject private var registerModel: RegisterModel
     
@@ -97,34 +77,27 @@ struct LoginView: View {
                     .textInputAutocapitalization(.never)
                     
                     
-                    HStack(spacing: 20) {
-                        // 애플 버튼
-                        HStack{
-                            Image(systemName: "applelogo")
-                                .resizable()
-                            
-                                .aspectRatio(contentMode: .fit)
-                                .frame(width: 25, height: 25)
-                                .frame(height: 45)
-                            
-                            Text("Apple Sign in")
-                                .font(.callout)
-                                .lineLimit(1)
+                    SignInWithAppleButton { request in
+                        registerModel.nonce = randomNonceString()
+                        request.requestedScopes = [.fullName, .email]
+                        request.nonce = sha256(registerModel.nonce)
+                    } onCompletion: { (result) in
+                        switch result {
+                        case .success(let user):
+                            print("success")
+                            guard let credential = user.credential as?
+                                    ASAuthorizationAppleIDCredential else {
+                                print("error with firebase")
+                                return
+                            }
+                            Task { await registerModel.appleAuthenticate(credential: credential) }
+                        case.failure(let error):
+                            print(error.localizedDescription)
                         }
-                        .padding(.horizontal,15)
-                            .foregroundColor(.white)
-                            .background {
-                                RoundedRectangle(cornerRadius: 10, style: .continuous)
-                                    .fill(.black)
-                            }
-                            .overlay {
-                                SignInWithAppleButton(.signIn, onRequest: configure, onCompletion: handle)
-                                    .blendMode(.overlay)
-                            }
-                            .clipped()
-                        
-                        
                     }
+
+                    .frame(width: 300, height: 45)
+
                 
                     
                 }
@@ -135,42 +108,6 @@ struct LoginView: View {
         .accentColor(Color("Red"))
     }
     
-    func configure(_ request: ASAuthorizationAppleIDRequest) {
-        request.requestedScopes = [.fullName, .email]
-//        request.nonce = ""
-    }
-    
-    func handle(_ authResult: Result<ASAuthorization, Error>) {
-        switch authResult {
-        case .success(let auth):
-            print(auth)
-            switch auth.credential {
-            case let appleIdCredentials as ASAuthorizationAppleIDCredential:
-                if let appleUser = AppleUser(credentials: appleIdCredentials),
-                   let appleUserData = try? JSONEncoder().encode(appleUser) {
-                    UserDefaults.standard.setValue(appleUserData, forKey: appleUser.userId)
-                    
-                    print("saved apple user", appleUser)
-                } else {
-//                    print("missing some fields", appleIdCredentials.email, appleIdCredentials.fullName, appleIdCredentials.user)
-                    
-                    guard
-                        let appleUserData = UserDefaults.standard.data(forKey: appleIdCredentials.user),
-                        let appleUser = try? JSONDecoder().decode(AppleUser.self, from: appleUserData)
-                    else { return }
-                    
-                    print(appleUser)
-                }
-                
-            default:
-                print(auth.credential)
-            }
-            
-        case .failure(let error):
-            print(error)
-        }
-    }
-
 }
 
 struct LoginView_Previews: PreviewProvider {
