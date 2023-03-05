15533e178f732d2a5ab818be31023970bdf5b8ec
Sehoon Jeong <wtd120120@gmail.com>
2023-03-06 00:08:49 +0900

    [Feat] didSelect(), Image Resizing, setupImage 기능 추가 #32
    - mapView( ~ , didSelect view: )
    - UIImageExtension
    - setupImage

--- 길가온/Gilgaon/Gilgaon/NewGilgaon/UserMapView.swift
+++ 길가온/Gilgaon/Gilgaon/NewGilgaon/UserMapView.swift
@@ -14,34 +14,44 @@ import MapKit
 struct UserMapView: UIViewRepresentable {
     @ObservedObject var flowerMapViewModel: FlowerMapViewModel
     
-    var landmarks:[LandmarkAnnotation] = LandmarkAnnotation.mockData
+    var landmarks:[LandmarkAnnotation] = []
     
-    //  "Description - Replace the body with a make UIView(context:) method that creates and return an empty MKMapView"
+    // func makeUIView() == func body() -> some View {}
     func makeUIView(context: Context) -> MKMapView {
         let mapView = MKMapView()
+        mapView.register(MKAnnotationView.self, forAnnotationViewWithReuseIdentifier: "custom")
         setSubscriber(mapView)
         return mapView
     }
-
+    
+    
+    // MARK: - mapView에 "다음장소"버튼 클릭 시, 맵뷰의 Region을 변경
     func setSubscriber( _ mapView: MKMapView){
         flowerMapViewModel.buttonCancellable = flowerMapViewModel.$nextBtnPressed.sink { _ in
-                mapView.setRegion(flowerMapViewModel.mapRegions, animated: true)
+            mapView.setRegion(flowerMapViewModel.mapRegions, animated: true)
         }
     }
     
     func updateUIView(_ uiView: MKMapView, context: Context) {
         uiView.delegate = context.coordinator
+        uiView.setRegion(flowerMapViewModel.mapRegions, animated: true)
         uiView.addAnnotations(makePins())
     }
     
     func makeCoordinator() -> UserMapViewCoordinator {
         UserMapViewCoordinator(mapViewController: self)
     }
-  
+    
+    // mapView에 표시할 Pin생성.
     func makePins() -> [LandmarkAnnotation]{
         var lAT:[LandmarkAnnotation] = []
         for location in flowerMapViewModel.locations{
-            lAT.append(LandmarkAnnotation(title: "", subtitle: "", coordinate: location.coordinate))
+            if location.id == flowerMapViewModel.mapLocation.id{
+                lAT.append(LandmarkAnnotation(markerId: location.id, isSelected: true,coordinate: location.coordinate))
+            }else{
+                lAT.append(LandmarkAnnotation(markerId: location.id, isSelected: false, coordinate: location.coordinate))
+            }
+            
         }
         return lAT
     }
@@ -49,24 +59,20 @@ struct UserMapView: UIViewRepresentable {
 
 // pin annotation
 final class LandmarkAnnotation: NSObject, MKAnnotation{
-    let markerId = UUID().uuidString
+    let markerId: String?
     var title:String?
     var subtitle: String?
+    var isSelected = false
     let coordinate: CLLocationCoordinate2D
-    static let mockData = [
-        LandmarkAnnotation(title: "ff", subtitle: "ggg", coordinate: CLLocationCoordinate2D(latitude: 37.562959200045654, longitude: 126.83252062990505)),
-        LandmarkAnnotation(title: "jj", subtitle: "ggg", coordinate: CLLocationCoordinate2D(latitude: 37.52014908427796, longitude: 126.75334198579641)),
-        LandmarkAnnotation(title: "kk", subtitle: "ggg", coordinate: CLLocationCoordinate2D(latitude: 37.54053029363062, longitude: 126.88217608491355)),
-        LandmarkAnnotation(title: "ll", subtitle: "ggg", coordinate: CLLocationCoordinate2D(latitude: 37.4802505761054, longitude: 126.86309586052927 )),
-        LandmarkAnnotation(title: "oo", subtitle: "ggg", coordinate: CLLocationCoordinate2D(latitude: 37.46442606194599, longitude:  126.89567907400533 ))
-    ]
-    init(title: String?, subtitle: String?, coordinate: CLLocationCoordinate2D) {
+    
+    init(markerId: String?, title: String? = nil, subtitle: String? = nil, isSelected: Bool = false, coordinate: CLLocationCoordinate2D) {
+        self.markerId = markerId
         self.title = title
         self.subtitle = subtitle
+        self.isSelected = isSelected
         self.coordinate = coordinate
     }
 }
-
 
 class UserMapViewCoordinator: NSObject, MKMapViewDelegate{
     var mapViewController: UserMapView
@@ -76,32 +82,45 @@ class UserMapViewCoordinator: NSObject, MKMapViewDelegate{
         self.mapViewController = mapViewController
     }
     
-    
-    
-    // mapAnnotationSelected
+    // mapAnnotation 클릭 시, 해당 annotation으로 region 이동.
     func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
+        guard let annotation =  view.annotation,
+              let ViewAnnotation = annotation as? LandmarkAnnotation else { return }
         
+        for location in mapViewController.flowerMapViewModel.locations{
+            if ViewAnnotation.markerId! == location.id {
+                mapViewController.flowerMapViewModel.mapLocation = location
+                mapViewController.flowerMapViewModel.updateMapRegions(location)
+            }
+        }
     }
     
-    
+    func setUpImage(_ annotation: LandmarkAnnotation) -> UIImage?{
+        var image = UIImage(named: "flowerPink")
+        image = image?.resized(to: CGSize(width: 40, height: 40))
+        return image
+    }
     
     func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
-        let annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: "custom")
-        let annotationimageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 30, height: 30))
-        let image = UIImage(named: "flowerPink")
-        annotationimageView.image = image
-        let annotationLabel = UILabel(frame: CGRect(x: 0, y: -35, width: 45, height: 15))
-        annotationLabel.backgroundColor = .systemOrange
-        annotationLabel.textColor = .white
-        annotationLabel.numberOfLines = 3
-        annotationLabel.textAlignment = .center
-        annotationLabel.font = UIFont.boldSystemFont(ofSize: 10)
-        annotationLabel.text = annotation.title!
-        annotationView.addSubview(annotationimageView)
-        annotationView.addSubview(annotationLabel)
-        return annotationView
+        guard let annotation = annotation as? LandmarkAnnotation else { return nil }
+        
+        guard let aView = mapView.dequeueReusableAnnotationView(withIdentifier: "custom") else {
+            let annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: "custom")
+            annotationView.image = setUpImage(annotation)
+            print("un reuse")
+            return annotationView
+        }
+        aView.image = setUpImage(annotation)
+        return aView
     }
-    
-    
 }
 
+
+//UIImage Renderer하기
+extension UIImage {
+    func resized(to size: CGSize) -> UIImage {
+        return UIGraphicsImageRenderer(size: size).image { _ in
+            draw(in: CGRect(origin: .zero, size: size))
+        }
+    }
+}
