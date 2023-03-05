913eef121792371e1f644ac03741eed4bcdbd205
JIN-JJS <joonsu2005@naver.com>
2023-03-05 14:55:55 +0900

    [Feat] 온보딩뷰 프로그래스바 추가

--- 길가온/Gilgaon/Gilgaon/View/OnBoard/OnboardingMapDescribeView.swift
+++ 길가온/Gilgaon/Gilgaon/View/OnBoard/OnboardingMapDescribeView.swift
@@ -4,28 +4,39 @@
 //
 //  Created by sehooon on 2022/12/22.
 //
-
+// MARK: -준수 수정함
 import SwiftUI
 
 struct OnboardingMapDescribeView: View {
+    
+    // MARK: Ready ProgressView
+    var progressReady : Double = 0.0
+    
+    // MARK: Start ProgressView
+    var progressStart: ClosedRange<Date> {
+        let start = Date()
+        let end = start.addingTimeInterval(5)
+        
+        return start...end
+    }
+    
+    // MARK: End ProgressView
+    var progressEnd: ClosedRange<Date> {
+        let start = Date()
+        let end = start.addingTimeInterval(0)
+        return start...end
+    }
+    
     var body: some View {
         
         ZStack {
-            Color("White")
-                .ignoresSafeArea()
-            LottieView2(filename: "Sakura")
-                .ignoresSafeArea()
-                .offset(x:200,y:-270)
-                .frame(width: 2000,height: 1300)
-                .opacity(0.6)
-       
             VStack(alignment: .center, spacing: 5) {
                 Spacer()
                 VStack {
-                        Text("꽃갈피")
+                    Text("꽃갈피")
                         .font(.custom("NotoSerifKR-Bold",size:40))
-                            .foregroundColor(Color("Pink"))
-                            .padding()
+                        .foregroundColor(Color("Pink"))
+                        .padding()
                 }
                 VStack() {
                     ZStack{
@@ -41,29 +52,54 @@ struct OnboardingMapDescribeView: View {
                             .offset(y:50)
                     }
                     .padding(.bottom,50)
-                    Text("“꽃갈피”란?")
-                      .kerning(-0.5)
-                      .font(.custom("NotoSerifKR-SemiBold",size:25))
-                      .foregroundColor(Color("Pink")) + Text(" 지나온 동선에 ")
-                      .kerning(-0.5)
-                      .foregroundColor(Color("DarkGray"))
-                      .font(.custom("NotoSerifKR-Light",size:25))
-                      
-                      
-    
                     
-                    Text("특별한 기록을 남겨주는 말입니다.")
-                        .padding(.top,5)
-                        .kerning(-0.5)
+                    VStack(spacing: 5) {
+                        Text("“꽃갈피”는")
+                            .kerning(-0.5)
+                            .font(.custom("NotoSerifKR-SemiBold",size:25))
+                            .foregroundColor(Color("Pink")) + Text(" 지나온 동선에 ")
+                            .kerning(-0.5)
+                            .foregroundColor(Color("DarkGray"))
+                            .font(.custom("NotoSerifKR-Light",size:25))
                         
+                        
+                        
+                        
+                        Text("특별한 기록을 남겨주는 말입니다.")
+                            .padding(.top,5)
+                            .kerning(-0.5)
+                    }
                     
                 }
                 .font(.custom("NotoSerifKR-Light",size:25))
                 .foregroundColor(Color("DarkGray"))
                 Spacer()
             }
+            
+            VStack {
+                Spacer()
+            HStack(spacing: 7) {
+                // MARK: End ProgressView
+                ProgressView(timerInterval: progressEnd, countsDown: false)
+                    .tint(Color("Pink"))
+                    .foregroundColor(.clear)
+                    .frame(width: 80)
+                
+                // MARK: Start ProgressView
+                ProgressView(timerInterval: progressStart, countsDown: false)
+                    .tint(Color("Pink"))
+                    .foregroundColor(.clear)
+                    .frame(width: 80)
+                
+                // MARK: Ready ProgressView
+                ProgressView(value: progressReady)
+                    .frame(width: 80)
+                    .padding(.bottom, 19)
+                
+            }
         }
-        
+        .frame(height: 500)
+        }
     
 
     }
