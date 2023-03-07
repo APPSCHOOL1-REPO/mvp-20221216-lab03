3922221635b02bd63006fd7e4ce3b2d187589ef2
stealmh <kmh922@naver.com>
2023-03-07 12:39:39 +0900

    [Fix] 머지 오류 수정

--- 길가온/Gilgaon/Gilgaon/NewGilgaon/DrawerView.swift
+++ 길가온/Gilgaon/Gilgaon/NewGilgaon/DrawerView.swift
@@ -28,8 +28,6 @@ struct DrawerView: View {
                     //서랍, 꽃갈피 뷰
                     DrawerDetailView(showMenu: $showMenu)
                         .frame(width: geometry.size.width, height: geometry.size.height)
-                    
-                    //                    GeometryReader { geometry in
                     HStack {
                         Spacer()
                         if showMenu {
@@ -38,42 +36,22 @@ struct DrawerView: View {
                                 .frame(width: geometry.size.width/2)
                                 .animation(.easeInOut(duration: 0.1), value: showMenu)
                                 .transition(.move(edge: .trailing)) //왼쪽에서 스윽 나옴
-                                .onTapGesture {
-                                    //햄버거 메뉴 뷰 터치하면 화면 안꺼지게
-                                    self.showMenu = true
-                                }
+                            //햄버거 메뉴 뷰 터치하면 화면 안꺼지게
+                                .onTapGesture { self.showMenu = true }
                         }
-                        
                     }
-                    //                        .offset(x: -65)
-                    Spacer()
-
-                }
-                .background(Color("DarkGray").opacity(showMenu ? 0.5 : 0))
-                .onTapGesture {
+                    .background(Color("DarkGray").opacity(showMenu ? 0.5 : 0))
                     //햄버거 메뉴 버튼말고 터치로 닫는 코드
-                    if self.showMenu {
-                        withAnimation {
-                            self.showMenu.toggle()
-                        }
-                        
-                    }
+                    .onTapGesture { if self.showMenu { self.showMenu.toggle() } }
                 }
-                //                    }
             }
             .onAppear {
-                //                myInfo(fireStoreViewModel.currentUserId!)
                 fireStoreViewModel.myInfo(fireStoreViewModel.currentUserId!)
                 fireStoreViewModel.fetchImageUrl()
                 fireStoreViewModel.fetchFriend()
             }
             .toolbar {
-                Button {
-//                    withAnimation {
-                        showMenu.toggle()
-//                    }
-                    
-                } label: {
+                Button(action: { showMenu.toggle() }) {
                     Image(systemName: "line.3.horizontal")
                 }
             }
