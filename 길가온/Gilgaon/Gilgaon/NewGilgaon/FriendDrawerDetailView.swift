d76fb940338419b67aa083aa11066f2470cbd766
JIN-JJS <joonsu2005@naver.com>
2023-03-12 22:03:53 +0900

    [Add] FriendDraweDetailView 생성 #48

---
+++ 길가온/Gilgaon/Gilgaon/NewGilgaon/FriendDrawerDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  FriendDrawerDetailView.swift
+//  Gilgaon
+//
+//  Created by 전준수 on 2023/03/12.
+//
+
+import SwiftUI
+
+struct FriendDrawerDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FriendDrawerDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        FriendDrawerDetailView()
+    }
+}
