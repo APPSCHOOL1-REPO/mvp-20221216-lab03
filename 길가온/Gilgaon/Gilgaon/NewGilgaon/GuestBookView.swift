434131356e428724289ddf203f192b68c4b900ee
jeongsoohee <dkssudgo365@naver.com>
2023-03-13 16:12:49 +0900

    [Feat] 방명록이 비어있을 경우 추가 #46

--- 길가온/Gilgaon/Gilgaon/NewGilgaon/GuestBookView.swift
+++ 길가온/Gilgaon/Gilgaon/NewGilgaon/GuestBookView.swift
@@ -17,89 +17,99 @@ struct GuestBookView: View {
     @State private var deleteToggle: Bool = false // ...버튼 누르고 삭제버튼 누르면 켜지는 토글
     @State private var showingAlert = false // ...버튼 누르고 삭제 or 신고버튼 누르면 다시한번 올라오는 얼럿용 토글
     @State private var alertType = AlertType.delete
-    var person: String = " "
+    var personImage: String = " " //user 프로필 이미지
+    var guestBook: String = "  " //방명록
     
     var body: some View {
         GeometryReader { geometry in
-            ZStack {
-                ScrollView {
-                    VStack {
-                        ForEach(1..<6) { _ in
-                            HStack(alignment: .top, spacing: 10) {
-                                
-                                if person.isEmpty {
-                                    //기본 프로필 이미지
-                                    Image(systemName: "person.circle.fill")
-                                        .resizable()
-                                        .frame(width: geometry.size.width/6.5, height: geometry.size.height/9.5)
-                                        .foregroundColor(Color("Pink"))
+            //빙명록이 비어있을 경우
+            if guestBook.isEmpty {
+                
+                guestBookIsEmptyTexts
+                
+            } else {
+                ZStack {
+                    ScrollView {
+                        VStack {
+                            ForEach(1..<6) { _ in
+                                HStack(alignment: .top, spacing: 10) {
                                     
-                                } else {
-                                    //사용자 프로필 이미지
-                                    Image("b02")
-                                        .resizable()
-                                        .clipShape(Circle())
-                                        .frame(width: geometry.size.width/6.5, height: geometry.size.height/9.5)
-                                }
-                                
-                                //방명록 쓴 사람의 정보 : 닉네임, 글, 작성시간
-                                guestInfo
-                                
-                                Spacer()
-                                
-                                // ...버튼
-                                Button(action: {
-                                    print("눌렀습니다")
-                                    ellipsisToggle.toggle()
-                                }) {
-                                    Image(systemName: "ellipsis")
-                                        .resizable()
-                                        .scaledToFit()
-                                        .foregroundColor(Color("Pink"))
-                                        .frame(width: 20, height: 20)
-                                }
-                                //하단에 뜨는 얼럿
-                                .confirmationDialog("해당 방명록이 불편하신가요?", isPresented: $ellipsisToggle, titleVisibility: .visible, presenting: alertType, actions: { type in
-                                    Button("삭제", role: .destructive) {
-                                        print("삭제하기")
-                                        alertType = .delete
-                                        showingAlert = true
-                                        deleteToggle = true
-                                    }
-                                    Button("신고", role: .destructive) {
-                                        print("신고하기")
-                                        alertType = .report
-                                        showingAlert = true
-                                    }
-                                    Button("취소", role: .cancel) {
-                                        print("신고하기")
-                                    }
-                                    
-                                })
-                                //삭제 or 신고 버튼을 누르면 가운데에 뜨는 얼럿
-                                .alert(deleteToggle ? "해당 방명록을 삭제하시겠습니까?" : "해당 방명록을 신고하시겠습니까?", isPresented: $showingAlert, presenting: alertType) { type in
-                                    
-                                    if type == .delete {
-                                        Button("삭제", role: .destructive) {}
-                                        Button("취소", role: .cancel) {}
+                                    if personImage.isEmpty {
+                                        //기본 프로필 이미지
+                                        Image(systemName: "person.circle.fill")
+                                            .resizable()
+                                            .frame(width: geometry.size.width/6.5, height: geometry.size.height/9.5)
+                                            .foregroundColor(Color("Pink"))
+                                        
                                     } else {
-                                        Button("신고", role: .destructive) {}
-                                        Button("취소", role: .cancel) {}
+                                        //사용자 프로필 이미지
+                                        Image("b02")
+                                            .resizable()
+                                            .clipShape(Circle())
+                                            .frame(width: geometry.size.width/6.5, height: geometry.size.height/9.5)
+                                    }
+                                    
+                                    //방명록 쓴 사람의 정보 : 닉네임, 글, 작성시간
+                                    guestInfo
+                                    
+                                    Spacer()
+                                    
+                                    // ...버튼
+                                    Button(action: {
+                                        print("눌렀습니다")
+                                        ellipsisToggle.toggle()
+                                    }) {
+                                        Image(systemName: "ellipsis")
+                                            .resizable()
+                                            .scaledToFit()
+                                            .foregroundColor(Color("Pink"))
+                                            .frame(width: 20, height: 20)
+                                    }
+                                    //하단에 뜨는 얼럿
+                                    .confirmationDialog("해당 방명록이 불편하신가요?", isPresented: $ellipsisToggle, titleVisibility: .visible, presenting: alertType, actions: { type in
+                                        Button("삭제", role: .destructive) {
+                                            print("삭제하기")
+                                            alertType = .delete
+                                            showingAlert = true
+                                            deleteToggle = true
+                                        }
+                                        Button("신고", role: .destructive) {
+                                            print("신고하기")
+                                            alertType = .report
+                                            showingAlert = true
+                                        }
+                                        Button("취소", role: .cancel) {
+                                            print("신고하기")
+                                        }
+                                        
+                                    })
+                                    //삭제 or 신고 버튼을 누르면 가운데에 뜨는 얼럿
+                                    .alert(deleteToggle ? "해당 방명록을 삭제하시겠습니까?" : "해당 방명록을 신고하시겠습니까?", isPresented: $showingAlert, presenting: alertType) { type in
+                                        
+                                        if type == .delete {
+                                            Button("삭제", role: .destructive) {}
+                                            Button("취소", role: .cancel) {}
+                                        } else {
+                                            Button("신고", role: .destructive) {}
+                                            Button("취소", role: .cancel) {}
+                                        }
                                     }
                                 }
-                            }
-                            .padding(.horizontal)
+                                .padding(.horizontal)
+                                
+                                Divider()
+                                    .background(Color.red)
+                            } //ForEach
                             
-                            Divider()
-                                .background(Color.red)
-                        } //ForEach
-                        
+                        }
+                        .padding(.vertical, 8)
                     }
-                    .padding(.vertical, 8)
                 }
             }
             //방명록 작성하는 버튼
-            Button(action: {}) {
+            Button(action: {
+                print("방명록 글쓰기 버튼 누름")
+            }) {
                 Circle()
                     .fill(Color("Pink"))
                     .frame(width: geometry.size.width/6.5, height: geometry.size.height/6.5)
@@ -118,6 +128,21 @@ struct GuestBookView: View {
 }
 
 extension GuestBookView {
+    
+    private var guestBookIsEmptyTexts: some View {
+        VStack {
+            Spacer()
+            Text("방명록이 비어있습니다!")
+                .font(.custom("NotoSerifKR-Bold", size: 17))
+            HStack {
+                Spacer()
+                Text("방명록을 남겨주세요!")
+                    .font(.custom("NotoSerifKR-Bold", size: 17))
+                Spacer()
+            }
+            Spacer()
+        }
+    }
     
     private var guestInfo: some View {
         VStack(alignment: .leading, spacing: 9) {
