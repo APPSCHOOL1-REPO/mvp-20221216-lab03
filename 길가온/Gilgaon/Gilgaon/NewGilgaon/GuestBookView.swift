bb0be52be2d79abbfbc91414f9323d3d2dac772a
jeongsoohee <dkssudgo365@naver.com>
2023-03-13 11:05:03 +0900

    [Add] GuestBookView(방명록뷰)생성 #46

---
+++ 길가온/Gilgaon/Gilgaon/NewGilgaon/GuestBookView.swift
@@ -0,0 +1,20 @@
+//
+//  GuestBookView.swift
+//  Gilgaon
+//
+//  Created by 정소희 on 2023/03/13.
+//
+
+import SwiftUI
+
+struct GuestBookView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct GuestBookView_Previews: PreviewProvider {
+    static var previews: some View {
+        GuestBookView()
+    }
+}
